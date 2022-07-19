801a3f11a1427ee140268a925e03e2bb49ac6bbe
maruthi <maruthivanguri@gmail.com>
2022-07-19 11:55:19 +0530

    allowance function added

--- ERC20/contracts/ERC20.sol
+++ ERC20/contracts/ERC20.sol
@@ -13,7 +13,7 @@ contract ERC20 is IERC20{
     address owner;
 
     mapping(address => uint256) internal balances;
-    mapping(address => mapping(address => uint256)) internal allowance;
+    mapping(address => mapping(address => uint256)) internal allowanceOf;
 
     constructor(string memory _name, string memory _Symbol, uint256 _supply, uint8 _decimal){
     //constructor(){
@@ -50,10 +50,10 @@ contract ERC20 is IERC20{
         require(_to != address(0),"Invalid Address to receive");
         require(_from != address(0),"Invalid Address to send");
         require(_value <= balances[_from],"Insufficient Balance" );
-        require(_value <= allowance[_from][msg.sender],"Not authorized to send requested amount");
+        require(_value <= allowanceOf[_from][msg.sender],"Not authorized to send requested amount");
         balances[_from] -= _value;
         balances[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
+        allowanceOf[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
@@ -69,9 +69,13 @@ contract ERC20 is IERC20{
     }
 
     function approve(address _spender, uint256 _value) public override returns (bool) {
-        allowance[msg.sender][_spender] = _value;
+        allowanceOf[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
+    }
+
+    function allowance(address _tokenOwner, address _spender) public override view returns (uint256) {
+        return allowanceOf[_tokenOwner][_spender];
     }
 
 
--- ERC20/contracts/IERC20.sol
+++ ERC20/contracts/IERC20.sol
@@ -10,7 +10,7 @@ interface IERC20 {
     function transfer(address _to, uint256 _value) external returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
     function approve(address _spender, uint256 _value) external returns (bool success);
-    //function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 
 
     event Transfer(address indexed from, address indexed to, uint256 tokens);
